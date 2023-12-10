695635445277eef2fb22f16b411e47bdb8dee241
WangHao <1046911101@qq.com>
2023-12-10 15:55:21 +0800

    [Normal] 20231210

--- StableDiffusion/stable-diffusion-webui-docker/services/AUTOMATIC1111/entrypoint.sh
+++ StableDiffusion/stable-diffusion-webui-docker/services/AUTOMATIC1111/entrypoint.sh
@@ -2,10 +2,13 @@
 
 set -Eeuo pipefail
 
-service ssh start
-
+# TODO: move all mkdir -p ?
 mkdir -p /data/config/auto/scripts/
 # mount scripts individually
+
+echo $ROOT
+ls -lha $ROOT
+
 find "${ROOT}/scripts/" -maxdepth 1 -type l -delete
 cp -vrfTs /data/config/auto/scripts/ "${ROOT}/scripts/"
 
@@ -30,12 +33,14 @@ declare -A MOUNTS
 
 MOUNTS["/root/.cache"]="/data/.cache"
 MOUNTS["${ROOT}/models"]="/data/models"
+
 MOUNTS["${ROOT}/embeddings"]="/data/embeddings"
 MOUNTS["${ROOT}/config.json"]="/data/config/auto/config.json"
 MOUNTS["${ROOT}/ui-config.json"]="/data/config/auto/ui-config.json"
 MOUNTS["${ROOT}/styles.csv"]="/data/config/auto/styles.csv"
 MOUNTS["${ROOT}/extensions"]="/data/config/auto/extensions"
 MOUNTS["${ROOT}/config_states"]="/data/config/auto/config_states"
+
 # extra hacks
 MOUNTS["${ROOT}/repositories/CodeFormer/weights/facelib"]="/data/.cache"
 
@@ -58,8 +63,15 @@ chown -R root ~/.cache/
 chmod 766 ~/.cache/
 
 shopt -s nullglob
+# For install.py, please refer to https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Developing-extensions#installpy
 list=(./extensions/*/install.py)
 for installscript in "${list[@]}"; do
+  EXTNAME=$(echo $installscript | cut -d '/' -f 3)
+  # Skip installing dependencies if extension is disabled in config
+  if $(jq -e ".disabled_extensions|any(. == \"$EXTNAME\")" config.json); then
+    echo "Skipping disabled extension ($EXTNAME)"
+    continue
+  fi
   PYTHONPATH=${ROOT} python "$installscript"
 done
 
